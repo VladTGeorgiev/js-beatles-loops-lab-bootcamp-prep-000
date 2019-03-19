4d459bc4e1fbd34a27ef4d59b8fcd8324cb293f2
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-19 23:17:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
   return foo
 }
 
-function johnLennonFacts ([
-"He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"
-])
+function johnLennonFacts (facts)
 {
   var checkedFacts = [];
   var i = 0;
