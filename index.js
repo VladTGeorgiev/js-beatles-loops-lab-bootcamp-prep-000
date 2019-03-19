5ccdb9e9d95e96a8524bd2722a8b549984e5e4e4
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-19 23:05:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const foo = []
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    foo.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return foo
+}
+
+function johnLennonFacts (facts) {
+  var foo = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+  var checkedFacts = [];
+  var i = 0;
+  while (i < checkedFacts.length) {
+    checkedFacts.push(`${checkedFacts[i]}!!!`);
+    i++;
+  }
+  return checkedFacts
+}
